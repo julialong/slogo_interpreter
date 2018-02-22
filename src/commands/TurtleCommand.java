25dc7d2a189eaa5fa823a93ab5e3f7acfca9e66f
Ben Hubsch <bah37@duke.edu>
2018-02-22 13:54:16 -0500

    Cleanup on TurtleCommand

--- src/commands/TurtleCommand.java
+++ src/commands/TurtleCommand.java
@@ -3,9 +3,6 @@ package commands;
 import slogo_team07.Turtle;
 
 public abstract class TurtleCommand implements Commandable {
-	
-	private Turtle myTurtle;
-	
 	protected Turtle getTurtle() {
 		return Interpreter.getInstance().getTurtle();
 	}
