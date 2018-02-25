14cfaec81910830cb607de309e7658a67af9f666
Maya Messinger <mm479@duke.edu>
2018-02-25 15:30:47 -0500

    change linking of engine

--- src/slogo_team07/Engine.java
+++ src/slogo_team07/Engine.java
@@ -3,7 +3,7 @@ package slogo_team07;
 import java.util.Map;
 import java.util.HashMap;
 
-import view.Canvas;
+import view.Visualizer;
 import model.Parser;
 import commands.CommandFactory;
 import commands.Commandable;
@@ -11,12 +11,12 @@ import commands.Result;
 
 public class Engine implements ChangeListener {
 
-	private Canvas myVis;
+	private Visualizer myVis;
 	private Map<String, Updatable> myUpdatables;
 	private Parser myParser;
 	private CommandFactory myCommandFactory;
 
-	public Engine(Canvas vis) {
+	public Engine(Visualizer vis) {
 		myVis = vis;
 		myUpdatables = new HashMap<>();
 		myCommandFactory = new CommandFactory(myUpdatables);
