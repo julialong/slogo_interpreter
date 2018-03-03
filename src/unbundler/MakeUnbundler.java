6f47c1943f9a1ca3e97b8608caeda1e53a6156de
Julia Long <julia.long@duke.edu>
2018-03-02 23:03:34 -0500

    DoTimes now accepts expressions and variables as limit

--- src/unbundler/MakeUnbundler.java
+++ src/unbundler/MakeUnbundler.java
@@ -11,61 +11,45 @@ import java.util.Map;
 public class MakeUnbundler extends ControlUnbundler{
 
     private List<String> expression;
-    private Map<String, String> dictionary;
-    private CommandFactory commandFactory;
-    private double answer;
+    private Map<String, String> dictionary;;
 
-    public MakeUnbundler(Map<String, String> dict, CommandFactory cf) {
-        dictionary = dict;
-        commandFactory = cf;
-    }
-
-    /**
-     * unbundles the given control command starting at index to crete a new variable
-     * @param exp is the entire ArrayList of the input commands
-     * @param index is the index that the control command was found
-     * @return the string variable name
-     */
-    public String unbundle(List<String> exp, int index) {
-        String variableName = exp.get(index + 1);
-        expression = new ArrayList<>();
-        buildExpression(exp, index + 1, exp.size());
-        executeExpression();
-        addVariable(variableName);
-        modifyList(exp, index, index + 1);
-        return "";
-    }
-
-    /**
-     * Builds the expression to be evaluated
-     * @param exp is the entire ArrayList of the input commands
-     * @return the index of the first left bracket
-     */
-    private void buildExpression(List<String> exp, int start, int end) {
-        for (int i = start + 1; i < end; i++) {
-            String current = exp.get(i);
-            System.out.println(current);
-            expression.add(current);
+        public MakeUnbundler(Map<String, String> dict) {
+            dictionary = dict;
         }
-    }
 
-    private void executeExpression() {
-        if (expression.size() <= 0) {
-            answer = 0;
-        } else {
-            Iterable<Commandable> iterable = new Parser(commandFactory).parse(String.join(" ", expression));
-            for (Commandable c : iterable) {
-                c.execute();
-                answer = c.getAns();
+        /**
+         * unbundles the given control command starting at index to crete a new variable
+         * @param exp is the entire ArrayList of the input commands
+         * @param index is the index that the control command was found
+         * @return the string variable name
+         */
+        public String unbundle(List<String> exp, int index) {
+            String variableName = exp.get(index + 1);
+            expression = new ArrayList<>();
+            buildExpression(exp, index + 2, exp.size() - 1);
+            addVariable(variableName);
+            modifyList(exp, index, index + 1);
+            return String.join(" ", expression);
+        }
+
+        /**
+         * Builds the expression to be evaluated
+         * @param exp is the entire ArrayList of the input commands
+         * @return the index of the first left bracket
+         */
+        private void buildExpression(List<String> exp, int start, int end) {
+            for (int i = start + 1; i < end; i++) {
+                String current = exp.get(i);
+                expression.add(current);
             }
         }
+
+        /**
+         * TODO: Unsure if this correctly modifies map
+         * @param variable string variable name
+         */
+        private void addVariable(String variable) {
+            dictionary.put(variable, String.join(" ", expression));
+        }
     }
 
-    /**
-     * TODO: Unsure if this correctly modifies map
-     * @param variable string variable name
-     */
-    private void addVariable(String variable) {
-        dictionary.put(variable, String.join(" ", Double.toString(answer)));
-    }
-}
--- src/unbundler/UnbundlerFactory.java
+++ src/unbundler/UnbundlerFactory.java
@@ -18,11 +18,11 @@ public class UnbundlerFactory {
 		if (lower.equals("repeat")) {
 			return new RepeatUnbundler(myCommandFactory);
 		} else if (lower.equals("dotimes")) {
-			return new DoTimesUnbundler();
+			return new DoTimesUnbundler(myCommandFactory);
 		} else if (lower.equals("for")) {
 			return new ForUnbundler();
 		} else if (lower.equals("make") || lower.equals("set")) {
-			return new MakeUnbundler(var_map, myCommandFactory);
+			return new MakeUnbundler(var_map);
 		} else if (lower.equals("to")) {
 			return new ToUnbundler(func_map);
 		} else if (lower.equals("if")) {
