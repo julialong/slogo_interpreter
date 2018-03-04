c078ea75b417473c889c8e4c4b4796ad9b2c7f2e
Julia Long <julia.long@duke.edu>
2018-03-04 15:06:28 -0500

    Start Reader class

--- src/parser/ContentReader.java
+++ src/parser/ContentReader.java
@@ -1,13 +1,17 @@
 package parser;
 
+import java.io.File;
+import java.util.Map;
+
 public class ContentReader {
 
     public ContentReader() {
 
     }
 
-    public void readVariables() {
-
+    public void readVariables(Map<String,String> vars, String fileName) {
+        File toRead = new File(fileName);
+        
     }
 
     public void readCommands() {
